dad28a7c0c98bedeceb24ad8369833bcb71fc6d1
Joshua Haberman <jhaberman@gmail.com>
2012-02-20 13:24:44 -0800

    Cleaned up and commented scrape.js.

--- app/scrape.js
+++ app/scrape.js
@@ -38,23 +38,19 @@ function Scrape(jsFile, scrapeletSym, params, onData) {
   var step = 0;
   var next = 1;
   function runNextStep() {
-    var action = scrapelet[step];
-    if (typeof(action) == "string") {
-      next = step + 1;
-      chrome.tabs.update(tabId, {"url": action});
-    } else if (typeof(action) == "function") {
-      var b = new Barrier()
-      chrome.tabs.executeScript(tabId, {"file": jsFile}, b.get());
-      chrome.tabs.executeScript(tabId, {"file": "scrape.js"}, b.get());
-      chrome.tabs.executeScript(tabId, {"file": "jquery.js"}, b.get());
-      b.run(function() {
-        console.log("Running scrapelet " + step);
-        var c = code + 'RunScrapelet(scrapelet, ' + step + ');';
-        chrome.tabs.executeScript(tabId, { "code": c });
-      });
-    } else {
+    if (typeof(scrapelet[step]) != "function") {
       throw typeof(step) + " " + step + "/" + scrapelet.length;
     }
+    // Load the scrapelet and other required files in the controlled tab.
+    // Once they have been loaded, run the appropriate step of the scrapelet.
+    var b = new Barrier()
+    chrome.tabs.executeScript(tabId, {"file": jsFile}, b.get());
+    chrome.tabs.executeScript(tabId, {"file": "scrape.js"}, b.get());
+    chrome.tabs.executeScript(tabId, {"file": "jquery.js"}, b.get());
+    b.run(function() {
+      var c = code + 'RunScrapelet(scrapelet, ' + step + ');';
+      chrome.tabs.executeScript(tabId, { "code": c });
+    });
   }
   chrome.tabs.onUpdated.addListener(function(t, changeInfo) {
     if (!next || t != tabId || changeInfo.status != "complete") return;
@@ -62,17 +58,21 @@ function Scrape(jsFile, scrapeletSym, params, onData) {
     next = undefined;
     if (step < scrapelet.length) runNextStep();
   });
+
+  // The controlled tab sends us messages to yield scraped data or to indicate
+  // completion of a step (and an indication of which step should be next).
   chrome.extension.onConnect.addListener(function(port) {
     if (!port.sender.tab || port.sender.tab.id != tabId) return;
     if (port.name == "meta") {
       port.onMessage.addListener(function(msg) {
-        console.log("Got ACK, next step should be: " + msg);
         next = msg;
       })
     } else {
       port.onMessage.addListener(function(msg) { onData(msg); });
     }
   });
+
+  // To kick off the scrapelet we load it and navigate to the start URL.
   $.getScript(jsFile, function() {
     eval(code);
     chrome.tabs.create({"url": scrapelet[0], "active": true}, function(tab) {
@@ -85,14 +85,17 @@ function Scrape(jsFile, scrapeletSym, params, onData) {
  * The code that runs inside the controlled browser's window.
  */
 function RunScrapelet(scrapelet, step) {
+  // Our goal is to run only after document.onload has run, but there appears
+  // to be no robust way to test this; see:
+  //   http://code.google.com/p/chromium/issues/detail?id=114890
+  //
+  // So our best effort is to wait until 100ms after document.readyState is
+  // set to "complete".
   function maybeReg() {
     if (document.readyState == "complete") reg();
     else setTimeout(maybeReg, 100);
   }
   function reg() {
-    // Our goal is to run only after document.onload has run, but there appears
-    // to be no robust way to test this; see:
-    //  http://code.google.com/p/chromium/issues/detail?id=114890
     setTimeout(doScrapelet, 100);
   }
   function doScrapelet() {
@@ -100,6 +103,8 @@ function RunScrapelet(scrapelet, step) {
     var dataPort = chrome.extension.connect();
     function send(val) { dataPort.postMessage(val); }
     var ret = scrapelet[step](send);
+    // Send a message back to our controlling window indicating completion and
+    // the next step to run.
     metaPort.postMessage(step + (typeof(ret) == "number" ? ret : 1));
   }
   maybeReg();
